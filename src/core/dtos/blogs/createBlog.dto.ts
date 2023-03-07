b253430e859981fcd1e46659fd2bc9a0380e02f1
dimaPolonez <dima-barxat@yandex.ru>
2023-03-08 00:30:50 +0300

    add trim decorator

--- src/core/dtos/blogs/createBlog.dto.ts
+++ src/core/dtos/blogs/createBlog.dto.ts
@@ -1,7 +1,18 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { trim } from '../../../helpers';
 
 export class CreateBlogDto {
+  @trim()
+  @Length(1, 15)
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
+  @Length(1, 500)
   description: string;
+
+  @IsNotEmpty()
+  @Length(1, 100)
   websiteUrl: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
 import { PORT_CONFIGURATION } from './configuration';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(PORT_CONFIGURATION.PORT_DEV);
   console.log(`Start server on ${process.env.PORT} port`);
 }
