6b150d0637e826842c1d876845c2ba3570205c36
dimaPolonez <dima-barxat@yandex.ru>
2023-03-09 14:11:32 +0300

    fix bags

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(PORT_CONFIGURATION.PORT_DEV);
   console.log(`Start server on ${process.env.PORT} port`);
