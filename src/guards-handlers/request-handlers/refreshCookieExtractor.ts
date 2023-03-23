7b1a31dbab28abb9e48ad5f2304318be031615f2
dimaPolonez <dima-barxat@yandex.ru>
2023-03-23 08:59:28 +0300

    log refresh

--- src/guards-handlers/request-handlers/refreshCookieExtractor.ts
+++ src/guards-handlers/request-handlers/refreshCookieExtractor.ts
@@ -2,6 +2,7 @@ export const RefreshCookieExtractor = function (req) {
   let token = null;
   if (req && req.cookies) {
     token = req.cookies['refreshToken'];
+    console.log(token);
   }
   return token;
 };
